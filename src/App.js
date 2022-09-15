a48758426062823e4856042a338e1a35ce95b8d3
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-09-15 17:11:42 +0430

    add side bar structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.scss";
 import Contents from "./components/Contents";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div>
-      <Contents />
+    <div className="main-page">
+      <div className="nav-area">
+        <Navbar />
+      </div>
+      <div className="content-area">
+        <Contents />
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+const Navbar = () => {
+  return (
+    <div>
+      <div className="Logo">Logo</div>
+      <navbar>
+        <div className="sidebar">
+          <ul>
+            <li>one</li>
+            <li>Tow</li>
+            <li>Three</li>
+          </ul>
+        </div>
+        <div className="accordion">
+          <ul>
+            <li>one</li>
+            <li>Tow</li>
+            <li>Three</li>
+          </ul>
+        </div>
+      </navbar>
+    </div>
+  );
+};
+export default Navbar;
